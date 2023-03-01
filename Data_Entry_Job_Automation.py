86cf29847e3b7696bd641c2ef251a3f059a717db
utagg <126648429+ut81@users.noreply.github.com>
2023-03-01 12:54:11 +0530

    Update Data_Entry_Job_Automation.py

--- Data_Entry_Job_Automation.py
+++ Data_Entry_Job_Automation.py
@@ -8,8 +8,8 @@ from bs4 import BeautifulSoup
 import requests
 
 headers={
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70",
-    "Accept-Language":"en-US,en;q=0.9"
+    "User-Agent":"",
+    "Accept-Language":""
 }
 
 response=requests.get("https://www.zillow.com/san-francisco-ca/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22north%22%3A37.916810261970156%2C%22east%22%3A-122.23832167578125%2C%22south%22%3A37.633501230568804%2C%22west%22%3A-122.62833632421875%7D%2C%22mapZoom%22%3A11%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%7D",headers=headers)
@@ -51,7 +51,7 @@ import time
 
 google_form_link="your google form link"
 
-chrome_driver_path = Service("C:\Development\chromedriver.exe")
+chrome_driver_path = Service("")
 
 driver=webdriver.Chrome(service=chrome_driver_path)
 
